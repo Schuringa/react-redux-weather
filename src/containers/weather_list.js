aa2cddea45a77cb27fa1695ce7193f9e4a819fd8
jordans <schuringa.jordan@gmail.com>
2017-08-30 16:31:34 +1000

    Updated weather list to use chart component
    Added some styling

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Chart from '../components/chart.js'
 import PropTypes from 'prop-types'
 
 class WeatherList extends Component {
   renderWeather (cityData) {
     const name = cityData.city.name
+    // Converting from kelvin to celsius
+    const temps = cityData.list.map(weather => weather.main.temp - 273)
+    const pressures = cityData.list.map(weather => weather.main.pressure)
+    const humidities = cityData.list.map(weather => weather.main.humidity)
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Chart data={temps} color='orange' units='C' />
+        </td>
+        <td>
+          <Chart data={pressures} color='green' units='hPa' />
+        </td>
+        <td>
+          <Chart data={humidities} color='blue' units='%' />
+        </td>
       </tr>
     )
   }
@@ -19,9 +33,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (&#8451;)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
